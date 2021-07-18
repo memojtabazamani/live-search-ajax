dc2dbfc573d2eedf9d937be16c6c830a9e4ecb5f
memojtabazamani <memojtabazamani@gmail.com>
2021-07-18 23:57:20 +0430

    done: اضافه کردن انیمیشن هنگام جستجو جستجوی کتاب ها انجام میگیرد

--- script.js
+++ script.js
@@ -2,6 +2,11 @@
 const url        = "data.json";
 const typeAction = "GET";
 const wrapperEle = document.getElementById('items');
+const input      = document.getElementById('bookName');
+
+let results      = [];
+
+input.onkeyup = searchBook;
 
 // ajax 
 var request = new XMLHttpRequest();
@@ -12,11 +17,12 @@ request.onreadystatechange = function() {
 	if(request.readyState === 4 && request.status === 200) {
 		var output = "";
 		var jsonParser = JSON.parse(request.responseText);
+		results = jsonParser;
 		for (var i = 0; i < jsonParser.length; i++) {
 			output += 
-			`<div class="col-sm-4 mb-3">
+			`<div class="col-sm-4 mb-3 book">
 				<div class="card">
-				  <img class="card-img-top">
+				  <img class="card-img-top" src="images/` + jsonParser[i]["@id"] + `.jpg">
 				  <div class="card-body">
 				    <h5 class="card-title">` + jsonParser[i].title + `</h5>
 				    <p class="card-text">` + jsonParser[i].description +  ` </p>
@@ -40,10 +46,44 @@ request.onreadystatechange = function() {
 				</div>
 			</div>`;
 		}
-		console.log(output);
 		wrapperEle.innerHTML = output;
-		
 	}
+}
+
+function searchBook(e) {
+	var output = "";
+	wrapperEle.innerHTML = "";
+	let valueSearch = input.value;
+	for (var i = 0; i < results.length; i++) {
+		if(!results[i].title.includes(valueSearch)) continue;
+		output += 
+		`<div class="col-sm-4 mb-3 book">
+			<div class="card">
+			  <img class="card-img-top" src="images/` + results[i]["@id"] + `.jpg">
+			  <div class="card-body">
+			    <h5 class="card-title">` + results[i].title + `</h5>
+			    <p class="card-text">` + results[i].description +  ` </p>
+			  </div>
+			  <div class='card-footer'>
+			   <dl class="row">
+				  <dt class="col-sm-5">Author</dt>
+				  <dd class="col-sm-7">` + results[i].author + `</dd>
+
+				  <dt class="col-sm-5">Price</dt>
+				  <dd class="col-sm-7"><span class='badge badge-info badge-pill'>$` + results[i].price + `</span></dd>
+
+				  <dt class="col-sm-5">Genre</dt>
+				  <dd class="col-sm-7"><mark>` + results[i].genre + `</mark></dd>
+
+				  <dt class="col-sm-5">Publish Date</dt>
+				  <dd class="col-sm-7"><small>` + results[i].publish_date + `</small></dd>
+				</dl>
+			</div>		
+			 
+			</div>
+		</div>`;
+	}
+	wrapperEle.innerHTML = output;
 }
 
 request.send();
